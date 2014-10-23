c6dd635ebb721d17246db4a945469f7245a5dfd9
BillybobDonnie <don.g.firth@gmail.com>
2014-10-22 21:54:27 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -45,7 +45,7 @@ static const int GRID_COLUMNS = 10;
   // initialize Creatures
   for (int i = 0; i < GRID_ROWS; i++) {
     // this is how you create two dimensional arrays in Objective-C. You put arrays into arrays.
-  //  _gridArray[i] = [NSMutableArray array];
+    _gridArray[i] = [NSMutableArray array];
     x = 0;
 
     for (int j = 0; j < GRID_COLUMNS; j++) {
