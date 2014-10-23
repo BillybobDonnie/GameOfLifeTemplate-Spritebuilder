0c6fc531133a31ca237dade7b506fbfe86fe9f7d
BillybobDonnie <don.g.firth@gmail.com>
2014-10-22 21:46:10 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -49,10 +49,10 @@ static const int GRID_COLUMNS = 10;
     x = 0;
 
     for (int j = 0; j < GRID_COLUMNS; j++) {
-//      Creature *creature = [[Creature alloc] initCreature];
-//      creature.anchorPoint = ccp(0, 0);
-//      creature.position = ccp(x, y);
-//      [self addChild:creature];
+      Creature *creature = [[Creature alloc] initCreature];
+      creature.anchorPoint = ccp(0, 0);
+      creature.position = ccp(x, y);
+      [self addChild:creature];
 
       // this is shorthand to access an array inside an array
 //      _gridArray[i][j] = creature;
