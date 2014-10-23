fb3f9f038a965812dfd06f5a848901a8037530c4
BillybobDonnie <don.g.firth@gmail.com>
2014-10-22 21:32:26 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -35,10 +35,10 @@ static const int GRID_COLUMNS = 10;
   // divide the grid's size by the number of columns/rows to figure out the right width and height of each cell
   _cellWidth = self.contentSize.width / GRID_COLUMNS;
   _cellHeight = self.contentSize.height / GRID_ROWS;
-/*
+
   float x = 0;
   float y = 0;
-
+/*
   // initialize the array as a blank NSMutableArray
   _gridArray = [NSMutableArray array];
 
